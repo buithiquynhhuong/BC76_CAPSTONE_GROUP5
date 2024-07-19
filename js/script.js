e3968295bda0791a4f04af890641a75c1ca420fd
huynhpt <huong.23y0414@huemed-univ.edu.vn>
2024-07-20 00:29:35 +0700

    update code reponsive sections project and adjust css/js to navigation.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Truy cập button class 'navbar-toggler'
+  var toggleButton = document.querySelector(".navbar-toggler");
+
+  // Truy cập checkbox ID 'tranform_bg_nav'
+  var checkbox = document.getElementById("tranform_bg_nav");
+
+  // Lắng nghe sự kiện 'click' trên nút button
+  toggleButton.addEventListener("click", function () {
+    /* Thay đổi trạng thái của checkbox khi click vào 
+    button mặc định ban đầu là false sau mỗi lân click sẽ đổi trạng thái */
+    checkbox.checked = !checkbox.checked;
+  });
+});
