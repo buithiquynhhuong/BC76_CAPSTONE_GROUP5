c5c9f676a092e2907604bdc88ebdf9b25d938cd8
huynhpt <huong.23y0414@huemed-univ.edu.vn>
2024-07-29 23:56:04 +0700

    commit code for my team

--- js/script.js
+++ js/script.js
@@ -7,17 +7,15 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
-
 document.addEventListener("DOMContentLoaded", function () {
   // Truy cập button class 'navbar-toggler'
   let toggleButton = document.querySelector(".navbar-toggler");
-
   // Truy cập checkbox ID 'tranform_bg_nav'
   let checkbox = document.getElementById("tranform_bg_nav");
 
   // Lắng nghe sự kiện 'click' trên nút button
   toggleButton.addEventListener("click", function () {
-    /* Thay đổi trạng thái của checkbox khi click vào 
+    /* Thay đổi trạng thái của checkbox khi click vào
     button mặc định ban đầu là false sau mỗi lân click sẽ đổi trạng thái */
     checkbox.checked = !checkbox.checked;
   });
